524efe3622e7c517930acd7d05dd84ac39ee0792
Seu Nome <santanaemili@gmail.com>
2025-01-09 13:42:26 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Dictionary from './Dictionary';
+
 
 
 function App() {
   return (
     <div className="App">
+      <div container>
         <header className="App-header">
           <h1>My Dictionary</h1>
           <p>Find meanings and definitions for words!</p>
         </header>
         <main>
-        <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Search for a word:</label>
-    <input type="text" class="form-control w-25 mx-auto" id="exampleInputPassword1"></input>
-  </div>
-  <button className="btn" style={{ backgroundColor: '#04a29f', color: '#ffffff'}}>Search</button>
+       
+      
+      <Dictionary />
         </main>
         <footer className="App-footer">
           This project was coded by
           <a href="https://github.com/Emili-Santana" >
-          </a> and is  open-sourced on  <a 
-            href="https://github.com/Emili-Santana/my-dictionary-project"> GitHub </a> 
-           and    hosted on <a href="https://best-dictionary-react.netlify.app/" >
+          </a> and is  open-sourced on 
+           <a href="https://github.com/Emili-Santana/my-dictionary-project"> GitHub </a> 
+           and  hosted on <a href="https://best-dictionary-react.netlify.app/" >
           Netlify
           </a>
         </footer>
       </div>
- 
+      </div>
   );
 }
 
