6b8350fd857f489680f6eeeb1f7c874ee56b8d56
Seu Nome <santanaemili@gmail.com>
2025-01-09 10:59:54 +0000

    Add the header, main and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <header className="App-header">
+          <h1>My Dictionary</h1>
+          <p>Find meanings and definitions for words!</p>
+        </header>
+        <main>
+        <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Search for a word:</label>
+    <input type="text" class="form-control w-25 mx-auto" id="exampleInputPassword1"></input>
+  </div>
+  <button className="btn" style={{ backgroundColor: '#04a29f', color: '#ffffff'}}>Search</button>
+        </main>
+        <footer className="App-footer">
+          This project was coded by
+          <a href="https://github.com/Emili-Santana" >
+          </a> and is  open-sourced on  <a 
+            href="https://github.com/Emili-Santana/my-dictionary-project"> GitHub </a> 
+           and    hosted on <a href="https://best-dictionary-react.netlify.app/" >
+          Netlify
+          </a>
+        </footer>
+      </div>
+ 
   );
 }
 
