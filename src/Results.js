ff17704e54c0e1415336c4513d36ba529d3df194
Seu Nome <santanaemili@gmail.com>
2025-01-15 17:15:37 +0000

    Updated the component

--- src/Results.js
+++ src/Results.js
@@ -1,23 +1,32 @@
 import React from "react";
-import Meaning from './Meaning';
-import './Results.css';
+import Phonetics from "./Phonetic";
+import Meaning from "./Meaning";
+import "./Results.css";
 
-export default function Results (props){
-    console.log(props.results)
-if (props.results){
-return (<div className="Results">
-<h1 className="searching-word">{props.results.word}</h1>
-{props.results.meanings.map(function(meaning,index){
-return <div key={index}>
-    <Meaning meaning={meaning}/>
-
-    </div>
-})}
-
-</div>
-);
-} else {
-    return null;
-}
-
-}
+export default function Results(props) {
+    if (props.results) {
+      const phonetic = props.results.phonetics[0]; // Pega o primeiro item (se for o mesmo áudio)
+      return (
+        <div className="Results">
+          <section>
+            <h2 className="searching-word">{props.results.word}</h2>
+            {phonetic && (
+              <div>
+                <Phonetics phonetic={phonetic} />
+              </div>
+            )}
+          </section>
+          {props.results.meanings.map(function (meaning, index) {
+            return (
+              <section key={index}>
+                <Meaning meaning={meaning} />
+              </section>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+  
