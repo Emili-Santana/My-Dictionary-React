73f8160c56c29af9ed58ec43ece0023a18769ddb
Seu Nome <santanaemili@gmail.com>
2025-01-15 15:53:41 +0000

    updated component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,26 +1,32 @@
 import './Meaning.css';
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   const { meaning } = props;
-  console.log(props.meaning);
-
-  const firstDefinition = meaning.definitions && meaning.definitions[0];
+ 
+  const firstDefinition =  meaning.definitions[0];
 
   return (
+    
     <div className="Meaning">
       <h3 className="part-of-speech">{props.meaning.partOfSpeech}</h3>
  
            {firstDefinition && (
+            <>
         <div>
-          <p className='definition'>
+
+          <p className='definition'><strong>Definition: </strong>
             {firstDefinition.definition}
           </p>
-          {firstDefinition.example && (
-            <p className='example '>Ex.:
+         
+            <p className='example '><strong>Example: </strong>
               {firstDefinition.example}
             </p>
-          )}
-        </div>
+            
+            <Synonyms synonyms={props.meaning.synonyms} />
+            </div>
+            </>
+    
       )}
     </div>
   );
