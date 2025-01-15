ee6d5aa00fb0138e2b76d5671858f447e7a988bd
Seu Nome <santanaemili@gmail.com>
2025-01-15 17:15:13 +0000

    Criated and stylezed the compont

---
+++ src/Phonetic.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Phonetic.css";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <strong className="Pronunciation-title">Pronunciation: </strong>
+
+     
+
+      {props.phonetic.audio ? (
+        <a
+          href={props.phonetic.audio}
+          target="_blank"
+          rel="noreferrer"
+          className="audio"
+        >
+    🔊  
+        </a>
+      ) : (
+        <span className="audio">- No audio available</span>
+      )}
+
+<span className="text"> {props.phonetic.text} </span>
+    </div>
+  );
+}
